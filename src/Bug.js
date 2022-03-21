7c4fd5a69fc240e1f36e901ce29756254e1b6087
Tropie <tingoricki1@gmail.com>
2022-03-21 18:22:46 +0100

    Added delete button for bugs

--- src/Bug.js
+++ src/Bug.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import firebase from "./firebase";
 
 const BugContainer = styled.div`
     display: flex;
@@ -39,8 +40,27 @@ const BugDescription = styled.p`
     margin: 1.5rem 0;
 `
 
+const ButtonForDelete = styled.button`
+    align-self: flex-start;
+    border: none;
+    background-color: #577399;
+    color: #f7f7ff;
+    font-size: 1.1rem;
+    padding: 0.1rem 0.4rem;
+`
+
 function Bug(props) {
     const { bug } = props;
+
+    const deleteBug = async(id) => {
+        const snapshot = await firebase.firestore().collection("bugs")
+        .limit(1)
+        .where("id", "==", id)
+        .get()
+
+        const doc = snapshot.docs[0];
+        doc.ref.delete();
+    }
 
     return(
         <BugContainer>
@@ -50,6 +70,7 @@ function Bug(props) {
                 <UserName >Submitted by: {bug.submittedby}</UserName >
                 <ProfilePicture src={bug.userPhoto} alt="User who submitted the bug" />
             </UserInfo>
+            <ButtonForDelete onClick={() => deleteBug(bug.id)}>Delete</ButtonForDelete>
         </BugContainer>
     )
 }
